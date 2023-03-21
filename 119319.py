5a16f81b7122632bbbd53725315984c3a2ae5d4f
jt-rap1d <jt.sascyber16@gmail.com>
2023-03-21 12:31:57 +0700

    119319

--- 119319.py
+++ 119319.py
@@ -2,3 +2,4 @@
 print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
 print("No. Nama-Lengkap (NIM-Anda)")
 print("001. Cahyo Darujati (10097402)")
+print("002. Agung Jati (119319)")
