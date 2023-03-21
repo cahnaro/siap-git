14f4507220e9ccfffef6a06584a96c22a6e27403
zhayudin <zaenudinsatu@gmail.com>
2023-03-21 12:02:17 +0700

    116142
    116142

--- 116142.py
+++ 116142.py
@@ -1,3 +1,6 @@
 # Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
 print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
 print("No. Nama-Lengkap (NIM-Anda)")
+
+print("004. Mokhamad Zainuddin (116142)")
+
