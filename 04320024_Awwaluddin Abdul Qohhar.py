6306fb883adce79ffa122c1e27b1ead19760dabb
awlqohhr <qqohharr@gmail.com>
2023-03-23 11:37:12 +0700

    Menambahkan

---
+++ 04320024_Awwaluddin Abdul Qohhar.py
@@ -0,0 +1,4 @@
+# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
+print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
+print("No. Nama-Lengkap (NIM-Anda)")
+print("No.09 Awwaluddin Abdul Qohhar (04320024)")
