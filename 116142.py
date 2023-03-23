061ed5a856b8cfcbc03ee18286778eabd01f3acf
awlqohhr <qqohharr@gmail.com>
2023-03-23 12:08:38 +0700

    Update

--- 116142.py
+++
@@ -1,4 +0,0 @@
-# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
-print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
-print("No. Nama-Lengkap (NIM-Anda)")
-print("004. Mokhamad Zainuddin (116142)")
--- 119319.py
+++
@@ -1,4 +0,0 @@
-# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
-print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
-print("No. Nama-Lengkap (NIM-Anda)")
-print("001. Agung Jati (119319)")
