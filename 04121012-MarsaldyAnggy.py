936a36111e2d26a8e65e5426842a91a0f892933d
MarsaldyAnggy <128560570+MarsaldyAnggy@users.noreply.github.com>
2023-03-24 11:59:22 +0700

    Delete 04121012-MarsaldyAnggy.py

--- 04121012-MarsaldyAnggy.py
+++
@@ -1,5 +0,0 @@
-
-# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
-print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
-print("No. Nama-Lengkap (NIM-Anda)")
-print("No.000 Marsaldy Anggy (04121012)")
