24de7f967cb86d763dc70dea33b5d06dc766f380
alfin <alvinf889@gmail.com>
2023-03-27 22:44:42 +0700

    perbaiki isi file 04220019-AlfinFaizFebrianto

--- 04220019-AlfinFaizFebrianto.py
+++ 04220019-AlfinFaizFebrianto.py
@@ -1,1 +1,4 @@
-print("002. Rio Joan Pratama (04320002)")
+# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
+print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
+print("No. Nama-Lengkap (NIM-Anda)")
+print("000. Alfin Faiz Febrianto (04220019)")
